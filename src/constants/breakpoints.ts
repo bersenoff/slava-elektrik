9c63a5be8b36aef1709c1d02dfabfdec086e29fe
Никита Берсенев <bersenoff@bk.ru>
2023-11-12 23:48:10 +0800

    fix: mobile breakpoint

--- src/constants/breakpoints.ts
+++ src/constants/breakpoints.ts
@@ -1,5 +1,5 @@
 export enum BreakPoint {
-  Mobile = 'screen and (min-width: 360px) and (max-width: 767px)',
+  Mobile = 'screen and (max-width: 767px)',
   Tablet = 'screen and (min-width: 768px) and (max-width: 1365px)',
   TabletVertical = 'screen and (min-width: 768px) and (max-width: 1023px)',
   TabletHorizontal = 'screen and (min-width: 1024px) and (max-width: 1365px)',
