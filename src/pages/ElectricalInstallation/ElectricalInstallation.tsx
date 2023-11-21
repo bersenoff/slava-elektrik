5d91cc926c99b5312ea4ba72776b3fcb773aa50c
Никита Берсенев <bersenoff@bk.ru>
2023-11-21 17:13:55 +0800

    fix: electrical installation page

--- src/pages/ElectricalInstallation/ElectricalInstallation.tsx
+++ src/pages/ElectricalInstallation/ElectricalInstallation.tsx
@@ -6,12 +6,14 @@ import HowItWorks from "../Landing/components/HowItWorks";
 import Reviews from "../Landing/components/Reviews";
 import Section from "../Landing/components/Section";
 import Text, { TextType } from "../../components/Text";
-import Icon, { IconName } from "../../components/Icon";
 import { BreakPoint } from "../../constants/breakpoints";
 import useBreakpoints from "../../hooks/useBreakpoints";
+import ResponsiveWrapper from "../../components/ResponsiveWrapper";
+import Form from "../../entities/Form";
+import FormButton from "../../entities/FormButton";
 
 const TechConditions: React.FC = () => {
-  const { isMobile } = useBreakpoints();
+  const { isMobile, isDesktop } = useBreakpoints();
 
   const title = "Монтаж электрики под ключ в Иркутске";
   const description =
@@ -27,43 +29,77 @@ const TechConditions: React.FC = () => {
           content="Монтаж электропроводки, Монтаж проводки, Замена электропроводки, Замена электропроводки в квартире, Монтаж проводки в доме, Электромонтаж в квартире, Монтаж проводки в деревянном доме, Монтаж проводки в квартире, Стоимость замены электропроводки, Монтаж скрытой проводки, Электромонтаж проводки, Монтаж проводки в частном доме, Электромонтаж в частном доме, Монтаж кабеля, Замена электропроводки в панельном доме, Замена электропроводки в двухкомнатной квартире, Замена электропроводки в однокомнатной, Электромонтаж в квартире, Замена электропроводки в хрущевке"
         />
       </Helmet>
-      <Section bgColor="gray">
-        <Content>
-          <ImageContainer>
-            <img src="/img/electrical-installation.png" />
-          </ImageContainer>
-          <DescriptionContainer>
-            <Heading>
-              <Text type={TextType.H3} center medium>
-                {title}
+      <MainSection>
+        {isDesktop && (
+          <Content>
+            <ResponsiveWrapper>
+              <Form
+                title={
+                  <>
+                    Монтаж электрики
+                    <br />
+                    под ключ в Иркутске
+                  </>
+                }
+                hintText="ОСТАВЬТЕ ЗАЯВКУ И Я ПЕРЕЗВОНЮ ВАМ ЧЕРЕЗ 2 МИНУТЫ"
+                dark
+              />
+            </ResponsiveWrapper>
+          </Content>
+        )}
+      </MainSection>
+      {!isDesktop && (
+        <Section bgColor="white">
+          <Text type={TextType.H3} medium center>
+            {title}
+          </Text>
+          <Text type={TextType.H4}>{description}</Text>
+          <FormButton label="ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ" fullWidth={isMobile} />
+        </Section>
+      )}
+      <Section title="ВИДЫ РАБОТ" bgColor="gray">
+        <ListContainer>
+          <ListItem>
+            <ListItemImage>
+              <img src="/img/electrical-installation/zamena-provodki.jpg" />
+            </ListItemImage>
+            <ListItemDescription>
+              <Text type={TextType.H3} center>
+                Замена и монтаж проводки
               </Text>
-              <Text type={TextType.H4}>{description}</Text>
-            </Heading>
-            <Text type={TextType.H4} medium center={isMobile}>
-              Виды работ:
-            </Text>
-            <ListContainer>
-              <ListItem>
-                <Icon name={IconName.Check} />
-                <Text type={TextType.H5}>Замена и монтаж проводки</Text>
-              </ListItem>
-              <ListItem>
-                <Icon name={IconName.Check} />
-                <Text type={TextType.H5}>Прокладка кабеля</Text>
-              </ListItem>
-              <ListItem>
-                <Icon name={IconName.Check} />
-                <Text type={TextType.H5}>Монтаж, сборка электрощитов</Text>
-              </ListItem>
-              <ListItem>
-                <Icon name={IconName.Check} />
-                <Text type={TextType.H5}>
-                  Электромонтаж в квартирах, домах и офисных помещениях
-                </Text>
-              </ListItem>
-            </ListContainer>
-          </DescriptionContainer>
-        </Content>
+            </ListItemDescription>
+          </ListItem>
+          <ListItem>
+            <ListItemImage>
+              <img src="/img/electrical-installation/prokladka-kabelya.jpg" />
+            </ListItemImage>
+            <ListItemDescription>
+              <Text type={TextType.H3} center>
+                Прокладка кабеля
+              </Text>
+            </ListItemDescription>
+          </ListItem>
+          <ListItem>
+            <ListItemImage>
+              <img src="/img/electrical-installation/montazh-shitov.jpg" />
+            </ListItemImage>
+            <ListItemDescription>
+              <Text type={TextType.H3} center>
+                Монтаж, сборка электрощитов
+              </Text>
+            </ListItemDescription>
+          </ListItem>
+          <ListItem>
+            <ListItemImage>
+              <img src="/img/electrical-installation/elektromontazh.jpg" />
+            </ListItemImage>
+            <ListItemDescription>
+              <Text type={TextType.H3} center>
+                Электромонтаж в квартирах, домах и офисных помещениях
+              </Text>
+            </ListItemDescription>
+          </ListItem>
+        </ListContainer>
       </Section>
       <Discount />
       <HowItWorks />
@@ -72,98 +108,94 @@ const TechConditions: React.FC = () => {
   );
 };
 
+const MainSection = styled.div`
+  background: #f7f8f9 url("/img/electrical-installation.jpg") no-repeat;
+  background-size: 100% 140%;
+  box-shadow: inset 0 10px 10px rgba(255, 255, 255, 1);
+  padding-top: 47.5%;
+  position: relative;
+  width: 100%;
+
+  @media ${BreakPoint.Desktop} {
+    background-position: 0 -150px;
+  }
+
+  @media ${BreakPoint.Tablet} {
+    background-position: 0 -100px;
+  }
+
+  @media ${BreakPoint.Mobile} {
+    background-position: 0 -50px;
+  }
+`;
+
 const Content = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
   display: flex;
-  flex-direction: row;
   align-items: center;
-  gap: 64px;
-
-  @media ${BreakPoint.TabletHorizontal} {
-    gap: 32px;
-  }
-
-  @media ${BreakPoint.TabletVertical} {
-    flex-direction: column;
-    gap: 32px;
-  }
+  height: 100%;
+  width: 100%;
 
   @media ${BreakPoint.Mobile} {
-    flex-direction: column;
-    gap: 16px;
-  }
-`;
-
-const ImageContainer = styled.div`
-  width: 40%;
-
-  img {
-    border-radius: 10px;
-    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
-    width: 100%;
-  }
-
-  @media ${BreakPoint.TabletVertical} {
-    width: 60%;
-  }
-
-  @media ${BreakPoint.Mobile} {
-    width: 100%;
-  }
-`;
-
-const DescriptionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
-  width: 60%;
-
-  @media ${BreakPoint.TabletVertical} {
-    width: 100%;
-  }
-
-  @media ${BreakPoint.Mobile} {
-    gap: 16px;
-    width: 100%;
-  }
-`;
-
-const Heading = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-
-  @media ${BreakPoint.Mobile} {
-    gap: 12px;
+    position: static;
   }
 `;
 
 const ListContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   gap: 24px;
+  width: 100%;
 
   @media ${BreakPoint.Mobile} {
-    flex-direction: column;
-    gap: 12px;
+    grid-template-columns: 1fr;
   }
 `;
 
 const ListItem = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 4px;
+  background: white;
+  border: 1px solid rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
+  padding: 10px 10px 24px;
+  transition: all 0.3s ease;
 
-  svg {
-    color: green;
-    height: 30px;
-    width: 30px;
-
-    @media ${BreakPoint.Mobile} {
-      height: 20px;
-      width: 20px;
+  @media ${BreakPoint.Desktop} {
+    &:hover {
+      scale: 1.05;
     }
   }
+
+  @media ${BreakPoint.Mobile} {
+    margin: 0 auto;
+    max-width: 300px;
+    width: 100%;
+  }
+`;
+
+const ListItemImage = styled.div`
+  position: relative;
+  padding-top: 75%;
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const ListItemDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  border-top: 1px solid rgb(0, 0, 0, 0.25);
+  padding-top: 16px;
+  margin-top: 16px;
 `;
 
 export default TechConditions;
