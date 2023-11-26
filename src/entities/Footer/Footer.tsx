aa7957beddaee00b78293c4f4be4fd5c1c71463c
Никита Берсенев <bersenoff@bk.ru>
2023-11-26 16:19:57 +0800

    feat: add contacts

--- src/entities/Footer/Footer.tsx
+++ src/entities/Footer/Footer.tsx
@@ -9,6 +9,7 @@ import ResponsiveWrapper from "../../components/ResponsiveWrapper";
 import { BreakPoint } from "../../constants/breakpoints";
 import FormButton from "../FormButton";
 import Icon, { IconName } from "../../components/Icon";
+import Text, { TextType } from "../../components/Text";
 
 type Props = {
   formId?: string;
@@ -37,6 +38,66 @@ const Footer: React.FC<Props> = ({ formId }) => {
             </a>
           )}
         </Content>
+        <ContactsWrapper>
+          {isMobile && (
+            <ContactsLabel>
+              <Text type={TextType.H4} medium>
+                Архипов В. И.
+              </Text>
+            </ContactsLabel>
+          )}
+          <Contacts>
+            {!isMobile && (
+              <ContactItem>
+                <ContactItemLabel>
+                  <Text type={TextType.H5} medium>
+                    Архипов В. И.
+                  </Text>
+                </ContactItemLabel>
+              </ContactItem>
+            )}
+            <ContactItem>
+              <ContactItemLabel>
+                <Text type={TextType.H5} medium>
+                  ИНН
+                </Text>
+              </ContactItemLabel>
+              <ContactItemValue>
+                <Text type={TextType.H5}>382703063536</Text>
+              </ContactItemValue>
+            </ContactItem>
+            <ContactItem>
+              <ContactItemLabel>
+                <Text type={TextType.H5} medium>
+                  Адрес
+                </Text>
+              </ContactItemLabel>
+              <ContactItemValue>
+                <Text type={TextType.H5}>г. Иркутск, ул Шевцова, 68</Text>
+              </ContactItemValue>
+            </ContactItem>
+            {/* <ContactItem>
+              <ContactItemLabel>
+                <Text type={TextType.H5} medium>
+                  Индекс
+                </Text>
+              </ContactItemLabel>
+              <ContactItemValue>
+                <Text type={TextType.H5}>664035</Text>
+              </ContactItemValue>
+            </ContactItem> */}
+            {/* <ContactItem>
+              <ContactItemLabel>
+                <Text type={TextType.H5} medium>
+                  Часы работы
+                </Text>
+              </ContactItemLabel>
+              <ContactItemValue>
+                <Text type={TextType.H5}>Круглосуточно</Text>
+              </ContactItemValue>
+            </ContactItem> */}
+          </Contacts>
+        </ContactsWrapper>
       </ResponsiveWrapper>
     </StyledHeader>
   );
@@ -82,6 +143,50 @@ const StyledIcon = styled(Icon)`
       width: 60px;
     }
   }
+`;
+
+const ContactsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+  margin-top: 16px;
+  padding-top: 16px;
+  border-top: 1px solid rgba(0, 0, 0, 0.25);
+`;
+
+const ContactsLabel = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+const Contacts = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 24px;
+  width: 100%;
+`;
+
+const ContactItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  @media ${BreakPoint.Mobile} {
+    width: calc(50% - 24px);
+  }
+`;
+
+const ContactItemLabel = styled(ContactsLabel)`
+  font-size: 14px;
+`;
+
+const ContactItemValue = styled.div`
+  font-size: 14px;
+  font-weight: normal;
 `;
 
 export default Footer;
